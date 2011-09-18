6d36598200f94700aa5dfe7d7c45968ce01c9713
Dusan Novakovic <ndusan@gmail.com>
2011-09-19 01:29:44 +0200

    set small fix

--- app/views/layout/defaultLayout.php
+++ app/views/layout/defaultLayout.php
@@ -16,18 +16,19 @@
     </head>
     <body data-controller="<?= $this->_controller; ?>" data-method="<?= $this->_action; ?>">
         <!-- This is a content that will be included on page inside of this layout -->
-        <div id="jquiz">
-            <div class="quizWrapper">
-                <div class="quizTop">
-                    <a target="_blank" href="http://www.monograms.com"><img alt="monograms" title="Monograms - Independent Travel"src="../public/images/monograms.png" /></a>
-                </div>   
-                <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
-                    include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
-                <div class="quizBottom">
-                    <a target="_blank" href="http://www.visiteurope.com"><img alt="visit europe" title="visit europe" src="../public/images/logoVisiteurope.png" /></a>
-                    <a target="_blank" href="http://www.evernote.com"><img alt="evernote" title="evernote" src="../public/images/logoEvernote.png" /></a>
-                    <a class="pin" target="_blank" href="http://itunes.apple.com/us/app/evernote-peek/id442151267?mt=8"><img alt="evernote" title="evernote" src="../public/images/pin-and-paper.png" /></a>
-                </div>
+        
+        <div class="quizWrapper">
+            <div class="quizTop">
+                <a target="_blank" href="http://www.monograms.com"><img alt="monograms" title="Monograms - Independent Travel"src="../public/images/monograms.png" /></a>
+            </div> 
+            <div id="jquiz">
+            <? if (file_exists(VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'))
+                include (VIEW_PATH . $this->_controller . DS . $this->_action . 'View.php'); ?>
+            </div>
+            <div class="quizBottom">
+                <a target="_blank" href="http://www.visiteurope.com"><img alt="visit europe" title="visit europe" src="../public/images/logoVisiteurope.png" /></a>
+                <a target="_blank" href="http://www.evernote.com"><img alt="evernote" title="evernote" src="../public/images/logoEvernote.png" /></a>
+                <a class="pin" target="_blank" href="http://itunes.apple.com/us/app/evernote-peek/id442151267?mt=8"><img alt="evernote" title="evernote" src="../public/images/pin-and-paper.png" /></a>
             </div>
         </div>
         <div id="jquiz_loader"></div>
