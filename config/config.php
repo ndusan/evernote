18c9d38f7a748fd22cc91a303832d3f51c439311
emil <emil@ajduk.com>
2011-11-01 22:11:59 +0100

    asd

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ define('IN_DEV', true);
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'evernote');
 define('DB_USER', 'root');
-define('DB_PASS', 'root');
+define('DB_PASS', '');
 
 define('BASE_PATH', SITE.DS.APP_ROOT);
 
